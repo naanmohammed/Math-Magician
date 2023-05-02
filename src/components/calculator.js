6f1c89fe5b58bdb5e8f6c082a26907b4025e8bab
Naan Mohammed <naanahmed24@gmail.com>
2023-05-03 02:35:38 +0300

    Update calculator.js for events

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,42 +1,28 @@
+import React, { useState } from 'react';
 import './calculator.css';
+import calculate from '../logic/calculate';
+import ChildComponent from './childComponent';
 
-const Calculator = () => (
-  <div className="calculator-container">
-    <CalculatorInput />
-  </div>
-);
+const Calculator = () => {
+  const [calcData, setCalcData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-function CalculatorInput() {
+  const handleButtonClick = (buttonName) => {
+    const newData = calculate(calcData, buttonName);
+    setCalcData(newData);
+  };
+
   return (
-    <div className="calculator-input">
-      <input className="calculator-input-field" type="text" name="name" pattern="[0-9+\-*/(). ]+" placeholder="0" />
-      <div className="calculator-buttons">
-        <input className="calculator-button" type="button" value="AC" />
-        <input className="calculator-button" type="button" value="+/-" />
-        <input className="calculator-button" type="button" value="%" />
-        <input className="calculator-button math-button-orange" type="button" value="÷" />
-        <br />
-        <input className="calculator-button" type="button" value="7" />
-        <input className="calculator-button" type="button" value="8" />
-        <input className="calculator-button" type="button" value="9" />
-        <input className="calculator-button math-button-orange" type="button" value="x" />
-        <br />
-        <input className="calculator-button" type="button" value="4" />
-        <input className="calculator-button" type="button" value="5" />
-        <input className="calculator-button" type="button" value="6" />
-        <input className="calculator-button math-button-orange" type="button" value="-" />
-        <br />
-        <input className="calculator-button" type="button" value="1" />
-        <input className="calculator-button" type="button" value="2" />
-        <input className="calculator-button" type="button" value="3" />
-        <input className="calculator-button math-button-orange" type="button" value="+" />
-        <br />
-        <input className="calculator-button zero-button" type="button" value="0" />
-        <input className="calculator-button" type="button" value="." />
-        <input className="calculator-button math-button-orange" type="button" value="=" />
-      </div>
+    <div className="container">
+      <ChildComponent
+        calcData={calcData}
+        handleButtonClick={handleButtonClick}
+      />
     </div>
   );
-}
+};
 
 export default Calculator;
