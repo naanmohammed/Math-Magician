b47d72e5ee217b9ef309acd2b843f7e9c23b866f
Naan Mohammed <naanahmed24@gmail.com>
2023-05-03 03:10:19 +0300

    Add quotes styles to calculator.js and fix some styles

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './quotes.css';
 import './calculator.css';
 import calculate from '../logic/calculate';
 import ChildComponent from './childComponent';
