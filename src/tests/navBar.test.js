683b332ab72cb7dacfc20430a99d6ab40f6f9c04
Naan Ahmed Mohammed <naanahmed24@gmail.com>
2023-05-11 02:37:15 +0300

    Add tests for navBar component

---
+++ src/tests/navBar.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import NavBar from '../components/navBar';
+
+describe('NavBar', () => {
+  it('renders the component', () => {
+    render(<MemoryRouter><NavBar /></MemoryRouter>);
+    const navBarElement = document.querySelector('.navBar')
+    expect(navBarElement).toBeInTheDocument();
+  });
+
+  it('renders the navigation links', () => {
+    render(<MemoryRouter><NavBar /></MemoryRouter>);
+    const homeLink = screen.getByRole('link', { name: 'Home |' });
+    const calculatorLink = screen.getByRole('link', { name: 'Calculator |' });
+    const quoteLink = screen.getByRole('link', { name: 'Quote' });
+    
+    expect(homeLink).toBeInTheDocument();
+    expect(calculatorLink).toBeInTheDocument();
+    expect(quoteLink).toBeInTheDocument();
+  });
+});
